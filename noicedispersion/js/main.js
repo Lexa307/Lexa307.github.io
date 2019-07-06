a2567d51c9be44b88aa1c03a866d34e3558c9b08
Lexa307 <herodimon@yandex.ru>
2019-07-06 23:30:49 +0300

    wtf

--- noicedispersion/js/main.js
+++ noicedispersion/js/main.js
@@ -5,6 +5,246 @@ function bind(func, context) {
     return func.apply(context, arguments);
   };
 }
+
+function DispersionMaterial(parameters) {
+  THREE.ShaderMaterial.call(this);
+
+  this.envMap = null;
+  this.refractionRatio = 0.98;
+  this.lights = true;
+  this.isMeshBasicMaterial = true; // Force refreshUniformsCommon to be run
+  this.dispersionSamples = 10;
+  this.dispersion = 0.05;
+  this.dispersionBlendMultiplier = 20;
+  this.uWiggleScale =  0.5 ;
+  this.uWiggleDisplacement =  18.;
+  this.uWiggleSpeed = 0.33 ;
+  this.progress = 1.;
+  this.time = 0.;
+
+  var envmap_dispersion_pars_fragment = document.getElementById('envmap_dispersion_pars_fragment').textContent;
+  var envmap_dispersion_fragment = document.getElementById('envmap_dispersion_fragment').textContent;
+  var fragmentShader = THREE.ShaderLib.phong.fragmentShader;
+  var vertex_Shader = `#define PHONG
+  varying vec3 vViewPosition;
+  #ifndef FLAT_SHADED
+    varying vec3 vNormal;
+  #endif
+  
+  #include <envmap_pars_vertex>
+  #include <color_pars_vertex>
+  
+     	     
+  uniform float time;
+  varying vec2 vUv;
+  varying vec2 vUv1;
+  varying vec4 vPosition;
+  varying float tProgress;
+  uniform sampler2D texture1;
+  uniform sampler2D texture2;
+  uniform vec2 pixels;
+  uniform vec2 uvRate1;
+  uniform float progress;
+  attribute float offset;
+  attribute vec3 displacement;
+  uniform float refractionRatio;
+
+  varying vec3 worldPositions;
+  varying vec3 cameraToVertexs;
+  varying vec3 worldNormals;
+  
+     uniform float amplitude;
+
+
+
+precision highp float;
+precision highp int;
+
+varying vec3 vRefract;
+
+uniform float uWiggleScale;
+uniform float uWiggleDisplacement;
+uniform float uWiggleSpeed;
+uniform float uRefractionRatio;
+
+vec3 mod289(vec3 x) {
+return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 mod289(vec4 x) {
+return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 permute(vec4 x) {
+return mod289(((x*34.0)+1.0)*x);
+}
+
+vec4 taylorInvSqrt(vec4 r) {
+return 1.79284291400159 - 0.85373472095314 * r;
+}
+
+vec3 fade(vec3 t) {
+return t*t*t*(t*(t*6.0-15.0)+10.0);
+}
+// Classic Perlin noise
+float cnoise(vec3 P) {
+vec3 Pi0 = floor(P); // Integer part for indexing
+vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+Pi0 = mod289(Pi0);
+Pi1 = mod289(Pi1);
+vec3 Pf0 = fract(P); // Fractional part for interpolation
+vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+vec4 iy = vec4(Pi0.yy, Pi1.yy);
+vec4 iz0 = Pi0.zzzz;
+vec4 iz1 = Pi1.zzzz;
+
+vec4 ixy = permute(permute(ix) + iy);
+vec4 ixy0 = permute(ixy + iz0);
+vec4 ixy1 = permute(ixy + iz1);
+
+vec4 gx0 = ixy0 * (1.0 / 7.0);
+vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+gx0 = fract(gx0);
+vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+vec4 sz0 = step(gz0, vec4(0.0));
+gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+vec4 gx1 = ixy1 * (1.0 / 7.0);
+vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+gx1 = fract(gx1);
+vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+vec4 sz1 = step(gz1, vec4(0.0));
+gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+g000 *= norm0.x;
+g010 *= norm0.y;
+g100 *= norm0.z;
+g110 *= norm0.w;
+vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+g001 *= norm1.x;
+g011 *= norm1.y;
+g101 *= norm1.z;
+g111 *= norm1.w;
+
+float n000 = dot(g000, Pf0);
+float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+float n111 = dot(g111, Pf1);
+
+vec3 fade_xyz = fade(Pf0);
+vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
+return 2.2 * n_xyz;
+}
+  
+  mat4 rotationMatrix(vec3 axis, float angle) {
+    axis = normalize(axis);
+    float s = sin(angle);
+    float c = cos(angle);
+     float oc = 1.0 - c;
+
+    return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
+    oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
+    oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
+    0.0,                                0.0,                                0.0,                                1.0);
+    }
+
+vec3 rotatet(vec3 v, vec3 axis, float angle) {
+mat4 m = rotationMatrix(axis, angle);
+    return vec3((m * vec4(v, 1.0)).x,(m * vec4(v, 1.0)).y,(m * vec4(v, 1.0)).z);
+}
+  vec3 bezier4(vec3 a, vec3 b, vec3 c, vec3 d, float t) {    
+      return mix(mix(mix(a, b, t), mix(b, c, t), t), mix(mix(b, c, t), mix(c, d, t), t), t);
+  }
+  void main() {
+    #include <uv_vertex>
+    #include <uv2_vertex>
+    #include <color_vertex>
+    #include <beginnormal_vertex>
+    #include <morphnormal_vertex>
+    #include <skinbase_vertex>
+    #include <skinnormal_vertex>
+    #include <begin_vertex>
+    #include <morphtarget_vertex>
+    #include <skinning_vertex>
+    #include <displacementmap_vertex>
+    #include <project_vertex>
+    #include <logdepthbuf_vertex>
+    #include <clipping_planes_vertex>
+    vViewPosition = - mvPosition.xyz;
+    #include <worldpos_vertex>
+    #include <envmap_vertex>
+    #include <shadowmap_vertex>
+    #include <fog_vertex>
+    
+    float noise = cnoise(normalize(position) * uWiggleScale + ( time * uWiggleSpeed ) );//normalize(position)
+          vec3 pos = position - vec3(800.0,0.0,0.0) + normal * noise * vec3(uWiggleDisplacement);
+
+           worldPositions = (modelMatrix * vec4(pos, 1.0)).xyz;
+           cameraToVertexs = normalize(worldPositions - cameraPosition);
+           worldNormals = normalize(mat3(modelMatrix[0].xyz, modelMatrix[1].xyz, modelMatrix[2].xyz) * normal);
+
+          //vRefract = refract(cameraToVertex, worldNormal, refractionRatio);
+          vUv = uv;
+        
+          vec3 newPosition =  normal * amplitude* displacement;
+          //vFresnelNormal = normalize(normalMatrix * normal);
+          //vec4 modelPos = modelViewMatrix * vec4(pos+newPosition, 1.0);
+          //vFresnelPosition = modelPos.xyz;
+          
+                 vec3 newpos = position + vec3(-800.,0.,0.);
+                 tProgress = clamp(-1.,1., (progress - offset*0.001)/0.2 );
+                 newpos = mix(position, newpos, tProgress);
+                 newpos = bezier4(position, vec3(-400.,0.,1.) , vec3(-800.,0.,1.) ,newpos, tProgress);  
+                 
+                 gl_Position = projectionMatrix * modelViewMatrix * vec4( position+newpos*noise, 1.0 );//*nois
+      
+  
+  }
+  
+`
+  fragmentShader = fragmentShader.replace('#include <envmap_pars_fragment>', '#include <envmap_pars_fragment>\n' + envmap_dispersion_pars_fragment);
+  fragmentShader = fragmentShader.replace('#include <envmap_fragment>', envmap_dispersion_fragment);
+  
+  this.uniforms = THREE.UniformsUtils.clone( THREE.ShaderLib.phong.uniforms );
+  this.vertexShader = vertex_Shader;
+  this.fragmentShader = fragmentShader;
+
+  this.setValues( parameters );
+
+  this.uniforms.dispersion = {value: this.dispersion};
+  this.uniforms.dispersionBlendMultiplier = {value: this.dispersionBlendMultiplier};
+  this.uniforms.uWiggleDisplacement = {value:this.uWiggleDisplacement}
+  this.uniforms.uWiggleScale={value:this.uWiggleScale}
+  this.uniforms.uWiggleSpeed={value:this.uWiggleSpeed}
+  this.uniforms.progress = {  value:this.progress }
+  this.uniforms.time = {  value:this.time }
+  
+
+  this.defines.DISPERSION_SAMPLES = this.dispersionSamples;
+}
+
+DispersionMaterial.prototype = Object.create(THREE.ShaderMaterial.prototype);
+DispersionMaterial.prototype.constructor = DispersionMaterial;
+
 class Slider{
   constructor(selector){
     
@@ -20,20 +260,21 @@ class Slider{
     this.Count = 5;
     this.spheres = [];
     this.settings = {
-      refractionRatio: 0.92,
-      aberration: 0.1,
-      mirrorRefraction:  1 ,
+  
       progress: 1,
       animtime: 5,
       uWiggleScale:  0.5 ,
       uWiggleDisplacement: 18.,
       uWiggleSpeed:  0.33 ,
+      refractionRatio: 0.8,
+      dispersionSamples: 30,
+      dispersion: 0.8,
       bgcolor: "#"+ this.scene.background.getHexString ()
     }
     this.gui = new dat.GUI();
      this.gui.add(this.settings, 'refractionRatio', 0, 1, 0.001);
-     this.gui.add(this.settings, 'aberration', 0, 1, 0.001);
-     this.gui.add(this.settings, 'mirrorRefraction', 0, 1, 0.001);
+     this.gui.add(this.settings, 'dispersionSamples', 0, 50, 0.001);
+     this.gui.add(this.settings, 'dispersion', 0, 1, 0.001);
      this.gui.add(this.settings, 'progress', -5, 5, 0.001);
      this.gui.add(this.settings, 'uWiggleScale', 0.001, 1, 0.001);
      this.gui.add(this.settings, 'uWiggleDisplacement', 0.001, 30, 0.001);
@@ -66,45 +307,70 @@ class Slider{
     // this.animindex=0;
     // document.body.appendChild( this.animbtn );
     //this.animbtn.addEventListener( 'click', bind(this.animclick,this), false );
-    this.material = new THREE.ShaderMaterial({
-      extensions: {
-        derivatives: '#extension GL_OES_standard_derivatives : enable'
-      },
-      side: THREE.DoubleSide,
-      uniforms: {
-        refractionRatio: { type: 'f', value: 0 },
-        aberration: { type: 'f', value: 1  },
-        mirrorRefraction: { type: 'f', value:1 },
-        amplitude: { type: 'f', value:0.98 },
-        reflectionSampler: { value: new THREE.CubeTextureLoader()
-          //.setPath( 'textures/cubeMaps/' )
-          .load( [
+   this.material = new DispersionMaterial({ 
+    extensions: {
+      derivatives: '#extension GL_OES_standard_derivatives : enable'
+    },
+    side: THREE.DoubleSide,
+      envMap: new THREE.CubeTextureLoader()
+    .load( [
             'textures/cubeMaps/Frame.jpg',
             'textures/cubeMaps/Frame.jpg',
             'textures/cubeMaps/Frame.jpg',
             'textures/cubeMaps/Frame.jpg',
             'textures/cubeMaps/Frame.jpg',
             'textures/cubeMaps/Frame.jpg'
-          ] ) },
-        time: { type: 'f', value: 0 },
-        progress: { type: 'f', value: 1 },
-        uWiggleScale: { type: 'f', value: 0.001 },
-        uWiggleDisplacement: { type: 'f', value: 0.01 },
-        uWiggleSpeed: { type: 'f', value: 0.03 },
-      },
+    ] ),
+      refractionRatio: 0.8,
+      dispersionSamples: 30,
+      dispersion: 0.8,
+      time : 0 ,
+      progress: 1, 
+      uWiggleScale : 0.001 ,
+      uWiggleDisplacement :  0.01 ,
+      uWiggleSpeed : 0.03 ,
+  });
+  this.material.envMap.mapping = THREE.CubeRefractionMapping;
+  
+    // this.material = new THREE.ShaderMaterial({
+    //   extensions: {
+    //     derivatives: '#extension GL_OES_standard_derivatives : enable'
+    //   },
+    //   side: THREE.DoubleSide,
+    //   uniforms: {
+    //     refractionRatio: { type: 'f', value: 0 },
+    //     aberration: { type: 'f', value: 1  },
+    //     mirrorRefraction: { type: 'f', value:1 },
+    //     amplitude: { type: 'f', value:0.98 },
+    //     reflectionSampler: { value: new THREE.CubeTextureLoader()
+    //       //.setPath( 'textures/cubeMaps/' )
+    //       .load( [
+    //         'textures/cubeMaps/Frame.jpg',
+    //         'textures/cubeMaps/Frame.jpg',
+    //         'textures/cubeMaps/Frame.jpg',
+    //         'textures/cubeMaps/Frame.jpg',
+    //         'textures/cubeMaps/Frame.jpg',
+    //         'textures/cubeMaps/Frame.jpg'
+    //       ] ) },
+    //     time: { type: 'f', value: 0 },
+    //     progress: { type: 'f', value: 1 },
+    //     uWiggleScale: { type: 'f', value: 0.001 },
+    //     uWiggleDisplacement: { type: 'f', value: 0.01 },
+    //     uWiggleSpeed: { type: 'f', value: 0.03 },
+    //   },
       
-      // wireframe: true,
-       transparent: true,
-      vertexShader: document.getElementById( 'vertex' ).textContent,
-      fragmentShader: document.getElementById( 'fragment' ).textContent
-    });
+    //   // wireframe: true,
+    //    transparent: true,
+    //   vertexShader: document.getElementById( 'vertex' ).textContent,
+    //   fragmentShader: document.getElementById( 'fragment' ).textContent
+    // });
 
     this.controls = new THREE.OrbitControls( this.camera );
     this.camera.position.z = 1642;
     this.camera.position.set( 4125,  -500,  0);
     this.controls.update();
     this.bigtestgeometry=new THREE.IcosahedronGeometry(500, 4);
-    //bigtestgeometry.scale(  -1, 1, 1 );
+   // this.bigtestgeometry.scale(  -1, 1, 1 );
     let tessellateModifier = new THREE.TessellateModifier( 60 );
     tessellateModifier.modify( this.bigtestgeometry );
     this.bigtestgeometry = new THREE.BufferGeometry().fromGeometry( this.bigtestgeometry );
@@ -168,7 +434,7 @@ this.animate();
    
    
     console.log(cbtx);
-    this.material.uniforms.reflectionSampler.value = cbtx;
+    this.material.envMap = cbtx;
     
   }
 
@@ -176,9 +442,10 @@ this.animate();
     
 	  requestAnimationFrame( this.animate.bind(this) );
      
-       this.material.uniforms.refractionRatio.value = this.settings.refractionRatio;
-       this.material.uniforms.aberration.value = this.settings.aberration;
-       this.material.uniforms.mirrorRefraction.value = this.settings.mirrorRefraction;
+        this.material.refractionRatio = this.settings.refractionRatio;
+        this.material.dispersionSamples = this.settings.dispersionSamples;
+        this.material.dispersion = this.settings.dispersion;
+       
        this.time += 0.001;
        this.bigsphere.rotation.x+=0.001;
        this.material.uniforms.time.value = this.time;
